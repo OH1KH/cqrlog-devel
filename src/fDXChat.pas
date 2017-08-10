6f3c72d0e5d4742b27e5f0fff55fae4918d85d87
OH1KH <oh1kh@sral.fi>
2017-08-10 15:30:11 +0300

    DXChat bugfix. See README.OH1KH

--- src/fDXChat.pas
+++ src/fDXChat.pas
@@ -13,7 +13,10 @@ type
   { TfrmDXChat }
 
   TfrmDXChat = class(TForm)
+    btClear: TButton;
+    btClose: TButton;
     DXChatMemo: TMemo;
+    procedure btClearClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure FormHide(Sender: TObject);
@@ -36,11 +39,16 @@ implementation
 
 { TfrmDXChat }
 
-Uses dUtils,uMyini ;
+Uses dUtils,uMyini,dData ;
 
 procedure TfrmDXChat.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
    dmUtils.SaveWindowPos(frmDXChat);
+end;
+
+procedure TfrmDXChat.btClearClick(Sender: TObject);
+begin
+    CleanDXChatMemo;
 end;
 
 procedure TfrmDXChat.FormCreate(Sender: TObject);
@@ -57,8 +65,6 @@ end;
 procedure TfrmDXChat.FormShow(Sender: TObject);
 
 begin
-    DXChatMemo.Font.Name    := cqrini.ReadString('DXCluster','Font','DejaVu Sans Mono');
-    DXChatMemo.Font.Size    := cqrini.ReadInteger('DXCluster','FontSize',12);
     MaxLines := 50;
     dmUtils.LoadWindowPos(frmDXChat);
 end;
@@ -68,14 +74,13 @@ procedure TfrmDXChat.CleanDXChatMemo;
 Begin
      DXChatMemo.lines.Clear;
 end;
-
 procedure TfrmDXChat.FocusLastLine;
 begin
    with DXChatMemo do
      begin
       SelStart := GetTextLen;
       SelLength := 0;
-      ScrollBy(0, Lines.Count);
+      ScrollBy(0, Lines.Count+1);
       Refresh;
      end;
 end;
@@ -102,6 +107,7 @@ begin
   //remove "mycall de"
   l := length(cqrini.ReadString('Station', 'Call', ''))+4; //4 = ' DE '
   ChLine := FormatDateTime('hh:nn',Now)+'_'+copy(Chline,l+1,length(Chline)-l);
+  if dmData.DebugLevel>=1 then Writeln('Chat :',ChLine);
   DXChatMemo.lines.Add(ChLine);
   FocusLastLine;
  end;
--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,7 +4,7 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.1.0 (101)';
+  cVERSION    = '2.1.0 (102)';
   cMAJOR      = 2;
   cMINOR      = 0;
   cRELEAS     = 4;
