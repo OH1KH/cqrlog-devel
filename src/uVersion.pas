028d6c3a426e411ae3b517b0cecc7d6f8577d8ec
OH1KH <oh1kh@sral.fi>
2018-03-19 19:31:28 +0200

    CQmonitor/follow:double click on line loads std messages to wsjtx1.9.0rc3 and up

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.2.0 (122)';
+  cVERSION    = '2.2.0 (123)';
   cMAJOR      = 2;
   cMINOR      = 2;
   cRELEAS     = 0;
-  cBUILD      = 122;
-  cBUILD_DATE = '2018-02-13';
+  cBUILD      = 123;
+  cBUILD_DATE = '2018-03-19';
 
 implementation
 
