d8d64b8d70f37e2d469c7ba23a026cbd19d758ee
OH1KH <oh1kh@sral.fi>
2018-04-27 10:19:07 +0300

    CQ-monitor: Color selection update.Help update.Merged from master.

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.2.0 (137)';
+  cVERSION    = '2.2.0 (138)';
   cMAJOR      = 2;
   cMINOR      = 2;
   cRELEAS     = 0;
-  cBUILD      = 137;
-  cBUILD_DATE = '2018-04-19';
+  cBUILD      = 5;
+  cBUILD_DATE = '2018-04-27';
 
 implementation
 
